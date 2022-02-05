6b5d8f8cdd41cf743222691e54987c5ea31eb42a
Sorin Stancu <sorinpaul.stancu@yahoo.com>
2022-02-05 19:55:59 +0000

    first commit
    add api and swagger

---
+++ src/PokeDex.Api/Contracts/ApiRoutes.cs
@@ -0,0 +1,14 @@
+﻿namespace PokeDex.Api.Contracts
+{
+    public static class ApiRoutes
+    {
+        private const string PokemonNameRouteParameter = "pokemonName";
+
+        public static class Pokemon
+        {
+            public const string Root = "pokemon";
+            public const string GetByName = PokemonNameRouteParameter;
+            public const string GetTranslatedByName = "translated/" + PokemonNameRouteParameter;
+        }
+    }
+}
---
+++ src/PokeDex.Api/Controllers/PokemonController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using PokeDex.Api.Contracts;
+using PokeDex.Api.Domain;
+
+namespace PokeDex.Api.Controllers
+{
+    [Route(ApiRoutes.Pokemon.Root)]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet(ApiRoutes.Pokemon.GetByName)]
+        public async Task<IActionResult> GetPokemonAsync([FromRoute]string pokemonName, CancellationToken cancellationToken)
+        {
+            var response = new Pokemon
+            {
+                Name = "pikachu", Description = "electric pokemon", Habitat = "forest", IsLegendary = true
+            };
+
+            return Ok(response);
+        }
+
+        [HttpGet(ApiRoutes.Pokemon.GetTranslatedByName)]
+        public async Task<IActionResult> GetTranslatedPokemonAsync([FromRoute]string pokemonName, CancellationToken cancellationToken)
+        {
+            return Ok();
+        }
+    }
+}
